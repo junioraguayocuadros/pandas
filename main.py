831740f5f8f3ec8853ac78ccc51f42cffac2d512
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2020-01-21 23:26:05 -0500

    Printing panda series2

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-
+# SERIES
 series_test = pd.Series([100, 200, 300])
 print(series_test)
+print('- - - - - - - - - - - - ')
+
+series_test2 = pd.Series({1999: 48,
+                          2000: 65,
+                          2001: 89})
+print(series_test2)

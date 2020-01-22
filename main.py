5471456c3403458e157d8e7de4e8b4c46f25eef2
Junior Aguayo <junior.aguayo.cuadros@gmail.com>
2020-01-21 23:33:33 -0500

    Printing panda dataframes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-# SERIES
+# Series
+print('Series:')
 series_test = pd.Series([100, 200, 300])
 print(series_test)
 print('- - - - - - - - - - - - ')
@@ -9,3 +10,15 @@ series_test2 = pd.Series({1999: 48,
                           2000: 65,
                           2001: 89})
 print(series_test2)
+print('- - - - - - - - - - - - ')
+
+# DataFrames
+frame_test = pd.DataFrame({1999: [74, 38, 39],
+                           2000: [34, 32, 32],
+                           2011: [23, 39, 23]})
+print('DataFrames:')
+print(frame_test)
+print('- - - - - - - - - - - - ')
+
+
+
